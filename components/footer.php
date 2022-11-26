579c8ec89c0cd17b7ad582e89d6b19c3181e71e1
tivdaandrei <andrewr0gaming2@gmail.com>
2022-11-26 10:55:54 +0200

    cuaie moi

--- components/footer.php
+++ components/footer.php
@@ -10,6 +10,13 @@ sbfhkajsdbkfjabskfdjbaskfdjkajsbf<br>
 sdada
  </p>
       </div>
+      <div class="box">
+        <p style="color:white"> sbfhkajsdbkfjabskfdjbaskfdjkajsbf
+     
+sbfhkajsdbkfjabskfdjbaskfdjkajsbf<br>
+sdada
+ </p>
+      </div>
 
 
 </footer>
